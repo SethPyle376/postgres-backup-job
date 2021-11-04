808d892fb44096bf1f1a7924d8a03df10a5f0960
Seth Pyle <seth.pyle86@gmail.com>
2021-11-04 11:57:57 -0500

    initial commit

---
+++ do-backup.sh
@@ -0,0 +1,20 @@
+#file-name: postgres-backup.sh
+#!/bin/bash
+
+cd /home/root
+date1=$(date +%Y%m%d-%H%M)
+mkdir pg-backup
+PGPASSWORD="$PG_PASS" pg_dumpall -h postgresql-postgresql.devtroncd -p 5432 -U postgres -U postgres > pg-backup/postgres-db.tar
+file_name="pg-backup-"$date1".tar.gz"
+
+#Compressing backup file for upload
+tar -zcvf $file_name pg-backup
+
+notification_msg="Postgres-Backup-failed"
+filesize=$(stat -c %s $file_name)
+mfs=10
+if [[ "$filesize" -gt "$mfs" ]]; then
+# Uploading to s3
+aws s3 cp pg-backup-$date1.tar.gz $S3_BUCKET
+notification_msg="Postgres-Backup-was-successful"
+fi
