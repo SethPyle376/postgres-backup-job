79251008afeb3ca05067b5f257821cbe8e392339
Seth Pyle <seth.pyle@churchofjesuschrist.org>
2022-07-16 09:52:52 -0500

    Switched to nfs backup

--- do-backup.sh
+++ do-backup.sh
@@ -2,10 +2,9 @@
 #!/bin/bash
 
 cd /home/root
-date1=$(date +%Y%m%d-%H%M)
 mkdir pg-backup
 PGPASSWORD="$PG_PASS" pg_dumpall -h $PG_HOST -p $PG_PORT -U $PG_USER > pg-backup/postgres-db.sql
-file_name="pg-backup-"$date1".tar.gz"
+file_name="pg-backup.tar.gz"
 
 #Compressing backup file for upload
 tar -zcvf $file_name pg-backup
@@ -14,9 +13,9 @@ notification_msg="Postgres backup failed."
 filesize=$(stat -c %s $file_name)
 mfs=10
 if [[ "$filesize" -gt "$mfs" ]]; then
-# Uploading to s3
-aws s3 cp pg-backup-$date1.tar.gz $S3_BUCKET
-notification_msg="Postgres backup successful, compressed filesize was $filesize"
+pretty_filesize=$(echo $filesize | numfmt --to=iec-i)
+cp $file_name /backup
+notification_msg="Postgres backup successful, compressed filesize was $pretty_filesize"
 fi
 
 curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"$notification_msg\"}" $DISCORD_WEBHOOK
